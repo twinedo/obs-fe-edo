f1560fc9ca6be961eb63f4dbd6f4151963b77d97
Twin Edo <twin.e@wistkey.dev>
2024-03-18 15:39:25 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'obssolution.com.sg',
+			},
+		],
+	},
+};
 
 export default nextConfig;
